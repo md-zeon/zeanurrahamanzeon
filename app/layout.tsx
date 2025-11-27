24c885ae1f33d327c8eef41df32be4b23aefa361
md-zeon <zeon.cse@gmail.com>
2025-11-28 02:22:24 +0600

    Add custom font and SVG assets: include Brockmann font, frame, and hero background SVGs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const brockmann = localFont({
+  src: "./fonts/BrockmannVF.ttf",
+  variable: "--font-brockmann",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
-const geistMono = Geist_Mono({
+const robotoMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${brockmann.className} ${robotoMono.variable} antialiased`}
       >
         {children}
       </body>
