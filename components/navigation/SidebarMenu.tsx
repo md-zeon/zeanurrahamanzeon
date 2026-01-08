80281931f91e1cac9b8e58409440c0d2303a876e
md-zeon <zeon.cse@gmail.com>
2026-01-09 02:03:23 +0600

    Add LinkedIn link and enhance SidebarMenu layout with additional styling

--- components/navigation/SidebarMenu.tsx
+++ components/navigation/SidebarMenu.tsx
@@ -1,7 +1,10 @@
-import { LucideMenu } from "lucide-react"
+import { LucideLinkedin, LucideMenu } from "lucide-react"
 import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet"
 import Logo from "../Logo"
 import RollingLink from "../animations/RollingLink"
+import Image from "next/image"
+import DigitalClock from "../DigitalClock"
+import Link from "next/link"
 
 const SidebarMenu = () => {
     return (
@@ -10,14 +13,14 @@ const SidebarMenu = () => {
                 <LucideMenu className="cursor-pointer" />
             </SheetTrigger>
 
-            <SheetContent className="bg-[#F8FF31] max-w-3xl w-full sidebar-clip">
+            <SheetContent className="flex flex-col justify-between bg-[#F8FF31] max-w-3xl w-full sidebar-clip">
                 <SheetHeader>
-                    <SheetTitle>
+                    <SheetTitle className="p-4">
                         <Logo />
                     </SheetTitle>
                 </SheetHeader>
 
-                <nav className="flex flex-col gap-4 pt-12 pl-12 tracking-tighter uppercase text-7xl font-black font-primary">
+                <nav className="flex-1 flex flex-col gap-4 pt-12 pl-16 tracking-tighter uppercase text-7xl font-black font-primary">
                     <SheetClose asChild>
                         <RollingLink href="/">Ho<em>m</em>e</RollingLink>
                     </SheetClose>
@@ -31,6 +34,16 @@ const SidebarMenu = () => {
                         <RollingLink href="/contact">Cont<em className="-ml-3 mr-1">a</em>ct</RollingLink>
                     </SheetClose>
                 </nav>
+                <div className="pl-8 pr-4">
+                    <div className="flex gap-1 items-center justify-between pl-6">
+                        <div className="h-0.5 bg-black/80 flex-1" />
+                        <Image src="/icons/flower.svg" width={18} height={18} alt="Flower Icon" />
+                    </div>
+                    <div className="flex justify-between gap-4 items-center font-medium text-lg my-5">
+                        <Link href="https://www.linkedin.com/in/zeanur-rahaman-zeon" className="flex items-center gap-1 font-bold hover:underline px-4 pl-6" target="_blank w_full"><LucideLinkedin className="inline-block size-4" />/zeanur-rahaman-zeon</Link>
+                        <DigitalClock />
+                    </div>
+                </div>
             </SheetContent >
         </Sheet >
     )
