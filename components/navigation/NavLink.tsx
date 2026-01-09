08e66b182aae335402d9113df42a931465ca742a
md-zeon <zeon.cse@gmail.com>
2026-01-10 00:58:57 +0600

    added Footer component with LinkedIn link and integrate NavLink for navigation in footer

--- components/navigation/NavLink.tsx
+++ components/navigation/NavLink.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import Scramble from "../animations/Scramble";
+import { cn } from "@/lib/utils";
 
 interface Props {
     title: string;
     path: string;
+    isFooterLink?: boolean;
 }
 
-const NavLink = ({ title, path }: Props) => {
+const NavLink = ({ title, path, isFooterLink }: Props) => {
 
     return (
         <Scramble text={title}>
-            <Link href={path} className="py-3 px-4 rounded-sm border border-transparent hover:border-current transition-colors hover:bg-[#44444430] duration-300 ease-in">
+            <Link href={path} className={cn(isFooterLink ? "py-1" : "py-3 px-4", !isFooterLink && "rounded-sm border border-transparent hover:border-current transition-colors hover:bg-[#44444430] duration-300 ease-in")}>
                 {title}
             </Link>
         </Scramble>
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Logo from '../Logo'
+import Link from 'next/link'
+import { LucideLinkedin } from 'lucide-react'
+import NavLink from '../navigation/NavLink'
 
 const Footer = () => {
     return (
@@ -8,14 +12,37 @@ const Footer = () => {
                 <path d="M1 734C1.00001 458.764 1.00002 304.451 1.00003 29.2154L25 1" stroke="currentColor"></path>
             </svg>
             {/* Content */}
-            <div className='flex-1 h-full border-t-2'>
-                <h1>Footer Content</h1>
-            </div>
+            <div className='flex-1 h-full border-t-2 py-12'>
+                <div className='max-w-[1440px] mx-auto'>
+                    {/* Footer Header */}
+                    <section className='flex justify-between items-center border-b pb-2 relative'>
+                        <Logo />
+                        <Link href="https://www.linkedin.com/in/zeanur-rahaman-zeon" className="flex items-center gap-1 font-bold hover:underline px-4 pl-6 uppercase" target="_blank w_full"><LucideLinkedin className="inline-block size-4" />/zeanur-rahaman-zeon</Link>
+                        <div className='absolute -bottom-0.5 right-0 w-1/4 h-0.5 bg-white'></div>
+                    </section>
+
+                    {/* Footer Body */}
+                    <section className='py-16 uppercase'>
+                        {/* Overview */}
+                        <div>
+                            <h2 className='text-xl tracking-wide'>O<em>v</em>er<em>v</em>ie<em>w</em></h2>
+                            <nav>
+                                <ul className='mt-4 flex flex-col gap-2 font-medium text-sm'>
+                                    <NavLink title="Home" path="/" isFooterLink />
+                                    <NavLink title="About" path="/about" isFooterLink />
+                                    <NavLink title="Projects" path="/projects" isFooterLink />
+                                    <NavLink title="Contact" path="/contact" isFooterLink />
+                                </ul>
+                            </nav>
+                        </div>
+                    </section>
+                </div>
+            </div >
             {/* right border */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="100%" viewBox="0 0 18 734" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="100%" viewBox="0 0 18 734" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img" >
                 <path d="M17.0001 734L17 19.6102L1 1" stroke="currentColor"></path>
             </svg>
-        </footer>
+        </footer >
     )
 }
 
