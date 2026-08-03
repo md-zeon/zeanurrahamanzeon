3c10b09d62faa1ce057eb4344b240d80f289161e
md-zeon <zeon.cse@gmail.com>
2026-08-03 14:12:09 +0600

    phase-3 batch-4: services-section header leaf classes inline
    - ServicesSection: home-services_header-wrapper + home-services_header-top
      -> inline utilities (kept header/header-bottom/container as animation anchors,
      kept cards as @apply-composed / plain CSS per hybrid end-state)
    - Pruned 2 dead CSS rules
    - Build passes; emitted CSS shrinks

--- src/components/sections/ServicesSection.tsx
+++ src/components/sections/ServicesSection.tsx
@@ -17,19 +17,33 @@ export default function ServicesSection() {
   useSectionHeadings(ref);
 
   return (
-    <section id="services" data-parallax-type="ssection" className="section_home-services" ref={ref}>
+    <section
+      id="services"
+      data-parallax-type="ssection"
+      className="section_home-services"
+      ref={ref}
+    >
       <div className="padding-global is-bigger">
         <div className="container-large">
           <div className="home-services_component">
-            <div header-animation-type="container" className="home-services_header">
-              <div className="home-services_header-wrapper">
-                <div className="home-services_header-top">
-                  <h2 header-animation-type="heading-1" className="heading-style-h0">
-                    SERVICES &amp;
+            <div
+              header-animation-type="container"
+              className="home-services_header"
+            >
+              <div className="pt-[7rem] pb-[1.5rem] pr-[1.5rem]">
+                <div className="flex justify-start">
+                  <h2
+                    header-animation-type="heading-1"
+                    className="heading-style-h0"
+                  >
+                    SERVICES &
                   </h2>
                 </div>
                 <div className="home-services_header-bottom">
-                  <div header-animation-type="heading-2" className="heading-style-h0">
+                  <div
+                    header-animation-type="heading-2"
+                    className="heading-style-h0"
+                  >
                     EXPERTISE
                   </div>
                 </div>
@@ -38,14 +52,22 @@ export default function ServicesSection() {
             </div>
             <div className="home-services_layout">
               {services.map((service, i) => (
-                <div key={service.index} data-audio="https://bjornflow-assets.b-cdn.net/Audio/Card%20Hover.wav" className="home-services_card">
+                <div
+                  key={service.index}
+                  data-audio="https://bjornflow-assets.b-cdn.net/Audio/Card%20Hover.wav"
+                  className="home-services_card"
+                >
                   <div className="home-services_card-top">
                     <div className="home-services_card-header">
-                      <div className="text-caption-2 text-color-secondary">[{service.index}]</div>
+                      <div className="text-caption-2 text-color-secondary">
+                        [{service.index}]
+                      </div>
                       <div className="heading-style-h5">{service.title}</div>
                     </div>
                     <div className="home-services_card-p">
-                      <p className="text-size-regular text-color-secondary">{service.description}</p>
+                      <p className="text-size-regular text-color-secondary">
+                        {service.description}
+                      </p>
                     </div>
                   </div>
                   <div className="home-services_grid-list">
@@ -56,16 +78,27 @@ export default function ServicesSection() {
                     ))}
                   </div>
                   <div className="home-services_card-asset-wrapper">
-                    <div data-us-lazyload="true" data-us-project={service.usProject} className="home-services_card-asset">
+                    <div
+                      data-us-lazyload="true"
+                      data-us-project={service.usProject}
+                      className="home-services_card-asset"
+                    >
                       <video
                         muted
                         loop
                         playsInline
                         autoPlay
                         data-autoplay-on-scroll
-                        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
                       >
-                        <source src={SERVICE_ASSETS[i % SERVICE_ASSETS.length]} type="video/mp4" />
+                        <source
+                          src={SERVICE_ASSETS[i % SERVICE_ASSETS.length]}
+                          type="video/mp4"
+                        />
                       </video>
                     </div>
                   </div>
